bbf0acadef2444962f1fe66b69fcd808c0228cda
p0lygun <solankivibhakar82@gmail.com>
2023-01-09 01:00:43 +0530

    feat: add match statement to match guess and secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,27 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Welcome To Guessing Game!!!");
     println!("Guess the Number ");
 
-    let mut guess = String::new();  
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret_number is {secret_number}");
+    let mut guess = String::new();
     // mut is to make this var mutable
 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed To Read line");
 
-    println!("You gussed {guess}")
+    let guess: u32 = guess.trim().parse().expect("Please Type a number");
+
+    println!("You gussed {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => print!("Too Small"),
+        Ordering::Greater => print!("Too Large"),
+        Ordering::Equal => print!("You Win!!!"),
+    }
 }
