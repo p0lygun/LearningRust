c3bfb8c161115d3c1017511c5e65bc1802d1f09c
p0lygun <solankivibhakar82@gmail.com>
2023-01-09 00:39:15 +0530

    feat: take user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome To Guessing Game!!!");
+    println!("Guess the Number ");
+
+    let mut guess = String::new();  
+    // mut is to make this var mutable
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed To Read line");
+
+    println!("You gussed {guess}")
 }
